5e6ec775f5f651f258824e2abf5d73e68cd13e97
Jonathan SM Zea <jonathan.a.zea@ieee.org>
2025-01-10 15:18:37 -0500

    :tada: add least squares regression

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from .linear_syst_methods import eliminacion_gaussiana, descomposicion_LU, resolver_LU, matriz_aumentada, separar_m_aumentada  # type: ignore
+from .min_cuadrados import ajustar_min_cuadrados  # type: ignore
